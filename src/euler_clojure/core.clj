bbe5b9634b45a1a53d797c6cdcfa1d90b4a25bd8
Stanton Teters <stantont@gmail.com>
2011-02-10 03:07:40 -0500

    Code and tests for problem 3, largest prime factor for a number.

--- src/euler_clojure/core.clj
+++ src/euler_clojure/core.clj
@@ -1,7 +1,10 @@
-(ns euler-clojure.core)
+(ns euler-clojure.core
+  (:use [clojure.contrib.lazy-seqs :only [primes]])
+  (import [java.lang Math]))
 
 (defn multiple-of-any?
- "Determines if the number n is a multiple of any of the numbers in the vector ns."
+  "Determines if the number n is a multiple of any of the
+   numbers in the vector ns."
  [n xs]
  (if (some #(= 0 (mod n %)) xs) true false))
 
@@ -13,3 +16,21 @@
   numbers in the given vector"
  [xs limit]
  (reduce + (filter #(multiple-of-any? % xs) (range limit))))
+
+(defn fib-seq []
+  ((fn rfib [a b]
+     (cons a (lazy-seq (rfib b (+ a b)))))
+   0 1))
+
+(defn factors-loop
+  "Looks for each successive prime number as a factor.
+   Returns a vector of all prime factors."
+  [n]
+  (loop [n n factors [] curr-pos 0]
+    (let [p (nth primes curr-pos)
+          r (rem n p)]
+      (cond
+       (= n 1) factors
+       (= 0 r) (recur (/ n p) (conj factors p) curr-pos)
+       (> (* p p) n) (conj factors n)
+       :else (recur n factors (inc curr-pos))))))
--- test/euler_clojure/test/core.clj
+++ test/euler_clojure/test/core.clj
@@ -19,5 +19,24 @@
 (deftest test-sum-multiples-less-than
   (is (= 23 (sum-multiples-less-than [3, 5] 10))))
 
+(deftest test-fib-seq
+  (is (= [0 1 1 2 3 5 8 13 21 34] (take 10 (fib-seq)))))
+
+(deftest test-factors-loop
+  (are [factors n] (factors-loop n)
+       [2] 2
+       [2 2] 4
+       [3] 3
+       [5] 5
+       [3 3] 9
+       [2 2 3] 12))
+
 (deftest problem-001
   (is (= 233168 (sum-multiples-less-than [3 5] 1000))))
+
+(deftest problem-002
+  (is (= 4613732 (reduce + (filter even? (take-while #(< % 4000000) (fib-seq)))))))
+
+(deftest problem-003
+  (is (= [71 839 1471 6857] (factors-loop 600851475143)))
+  (is (= 6857 (apply max ( factors-loop 600851475143)))))
