84099ab7ec814169d0a003e24c3e5d2a262af6d5
Stanton Teters <stantont@gmail.com>
2011-05-12 13:12:40 -0400

    Revert to Clojure 1.2.
    Have issue that reverting fixed.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject euler-clojure "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
-                 [org.clojure.contrib/complete "1.3.0-alpha4"]
-                 ]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
