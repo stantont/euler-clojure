a94a930f82e3d22099bb14cf9e557447e9429d60
Stanton Teters <stantont@gmail.com>
2011-05-14 11:29:50 -0400

    Save one of many intermediate solutions that don't work when the number looked for is large.

--- src/euler_clojure/solutions.clj
+++ src/euler_clojure/solutions.clj
@@ -381,3 +381,31 @@ The product of these numbers is 26  63  78  14 = 1788696.
 53503534226472524250874054075591789781264330331690"
         val (subs (str (apply + (map bigint (str/split nums #"\s+")))) 0 10)]
     val))
+
+(defn problem-014
+  "The following iterative sequence is defined for the set of positive integers:
+
+   n ->  n/2 (n is even)
+   n ->  3n + 1 (n is odd)
+
+   Using the rule above and starting with 13, we generate the following sequence:
+
+   13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+   It can be seen that this sequence (starting at 13 and finishing at 1)
+   contains 10 terms. Although it has not been proved yet (Collatz Problem),
+   it is thought that all starting numbers finish at 1.
+
+   Which starting number, under one million, produces the longest chain?
+
+   NOTE: Once the chain starts the terms are allowed to go above one million."
+  []
+  (let [mem-f (memoize collatz)
+        vs (fn ([n] (conj (vec (take-while #(not= 1 %) (iterate collatz n))) 1)))
+        ;mem-vs (memoize vs)
+        compare (fn [a b] (if (> (count a) (count b))
+                           a b))
+        nums (range 999999 0 -2)
+        longest (reduce compare (map vs nums))]
+    ;(println (str longest))
+    ;(reduce compare (vs (first nums)) (next nums))
+    longest))
